85c0210128a6d46058b872e63e283631311781f4
yassineelmiri <yassireelhima@gmail.com>
2024-11-17 16:05:19 +0100

    Add backend server file: server.js

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// api end point
+app.use('/api/admin', adminRouter)
+app.use('/api/doctor', doctorRouter)
+app.use('/api/user', userRouter)
+
+
+app.get('/', (req, res) => {
+  res.send('Api working...')
+})
+
+app.listen(port, () => console.log('Server started', port)) 
